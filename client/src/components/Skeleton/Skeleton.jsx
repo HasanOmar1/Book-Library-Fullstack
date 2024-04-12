e374927f95d628cf4038660fb35fc54748eed905
HasanOmar1 <r3domar1@gmail.com>
2024-04-12 20:18:04 +0300

    made skeleton's width dynamic to window's width

--- client/src/components/Skeleton/Skeleton.jsx
+++ client/src/components/Skeleton/Skeleton.jsx
@@ -3,14 +3,22 @@ import Skeleton from "@mui/material/Skeleton";
 import Stack from "@mui/material/Stack";
 
 export default function SkeletonComp() {
+  const widthOfSkeleton =
+    window.innerWidth < 400
+      ? 350
+      : window.innerWidth < 450
+      ? 400
+      : window.innerWidth < 800
+      ? 700
+      : 1200;
+
   return (
     <Stack spacing={3}>
-      <Skeleton variant="rounded" width={1200} height={300} />
-      <Skeleton variant="rounded" width={1200} height={300} />
-      <Skeleton variant="rounded" width={1200} height={300} />
-      <Skeleton variant="rounded" width={1200} height={300} />
-      <Skeleton variant="rounded" width={1200} height={300} />
-      {/* <Skeleton variant="rectangular" width={310} height={100} /> */}
+      <Skeleton variant="rounded" width={widthOfSkeleton} height={300} />
+      <Skeleton variant="rounded" width={widthOfSkeleton} height={300} />
+      <Skeleton variant="rounded" width={widthOfSkeleton} height={300} />
+      <Skeleton variant="rounded" width={widthOfSkeleton} height={300} />
+      <Skeleton variant="rounded" width={widthOfSkeleton} height={300} />
     </Stack>
   );
 }
